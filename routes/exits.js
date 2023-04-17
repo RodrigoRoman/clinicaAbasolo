2b743676193a361ad0ab4408951de034f12b269c
RodrigoRomanDev <rodrigo.roman.developer@gmail.com>
2023-04-17 17:21:40 -0600

    grant create payment to caja

--- routes/exits.js
+++ routes/exits.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const exits = require('../controllers/exits');
 const catchAsync = require('../utils/catchAsync');
-const { isLoggedIn, isServAuthor,isDinamicDirectAdmin, validateService, validateSupply, validateHospital,validateExit,validatePayment} = require('../middleware');
+const { isLoggedIn, isServAuthor,isDinamicDirectAdmin, isDirectAdminOrCaja,validateService, validateSupply, validateHospital,validateExit,validatePayment} = require('../middleware');
 
 const multer = require('multer');
 
@@ -12,10 +12,10 @@ const Payment = require('../models/payment');
 
 
 router.route('/')
-    .get(isLoggedIn, isDinamicDirectAdmin,catchAsync(exits.index))
-    .post(isLoggedIn, isDinamicDirectAdmin,validatePayment, catchAsync(exits.createPayment))
+    .get(isLoggedIn, isDirectAdminOrCaja,catchAsync(exits.index))
+    .post(isLoggedIn, isDirectAdminOrCaja,validatePayment, catchAsync(exits.createPayment))
 
-router.get('/new', isLoggedIn, isDinamicDirectAdmin, exits.renderNewForm)
+router.get('/new', isLoggedIn, isDirectAdminOrCaja, exits.renderNewForm)
 
 router.route('/refill')
     .get(exits.refillForm)
@@ -31,8 +31,8 @@ router.route('/:id')
 router.get('/payments', isLoggedIn,isDinamicDirectAdmin, catchAsync(exits.index_payments))
 
 router.route('/hospital_account')
-    .get(exits.hospital_account)
-    .post(exits.accountReportPDF)
+    .get(isLoggedIn,isDirectAdminOrCaja,exits.hospital_account)
+    .post(isLoggedIn,isDirectAdminOrCaja,exits.accountReportPDF)
 
 router.get('/searchSP', isLoggedIn, isDinamicDirectAdmin, catchAsync(exits.servicesPayments))
 
